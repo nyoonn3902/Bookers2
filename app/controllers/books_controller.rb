45d7e729aacf64017619bd5025bd5bb0f2d96987
EC2 Default User <ec2-user@ip-172-31-9-131.ap-northeast-1.compute.internal>
2023-11-23 02:44:05 +0000

    35

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,35 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to book_path(book.id)
+  end
+
   def index
+    @book = Book.find(params[:id])
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.user_id = current_user.id
+    book.destroy
+    redirect_to books_path(book.id)
+  end
+
+private
+  def book_params
+    params.require(:book).permit(:title, :body )
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
- 
+
   def show
+    @users = User.all
+    @user = User.find(params[:id])
   end
 
+
   def edit
+  end
+
+  def index
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:title, :body )
   end
 end
