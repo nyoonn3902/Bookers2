0304090ec47ac3eb4efc29069603d26adea9af80
EC2 Default User <ec2-user@ip-172-31-9-131.ap-northeast-1.compute.internal>
2023-12-02 07:10:08 +0000

    2023/12/2 コメント機能追加前

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,9 +9,12 @@ class FavoritesController < ApplicationController
   
   def destroy
     book = Book.find(params[:book_id])
-    favorite = curent_user.favorites.find_by(book_id: book.id)
+    favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
     redirect_to book_path(book)
   end
+  
+  
+  
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
    resources :users, only: [:show, :edit, :index, :new, :update, :create]
-   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+   
+   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
    resource :favorite, omly: [:create, :destroy]
+   end
    root to: "homes#top"
    get 'home/about' => "homes#about",as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
